8827680a7dad9cae0c7fee2789c974dc91b7cbf3
saraheulie <79435002+saraheulie@users.noreply.github.com>
2021-03-22 15:18:46 -0500

    update

--- forms/CustomerUpdate/CustomerUpdate.js
+++ forms/CustomerUpdate/CustomerUpdate.js
@@ -1,4 +1,4 @@
-let newName = inptNewName.value
+/*let newName = inptNewName.value
     let oldName = inptOldName.value
     query = "SELECT * FROM pets WHERE `petName` = '" + oldName + "'"
     req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + databaseSchema + "&query=" + query)
@@ -16,3 +16,4 @@ let newName = inptNewName.value
                 lblMessage7.textContent = `Error: ${req.status}`
         }
     } 
+    */
