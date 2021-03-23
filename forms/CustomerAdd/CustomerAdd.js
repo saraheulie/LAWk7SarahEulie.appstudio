a2707c49b0f10bf9371853d1d5a304bba0304425
saraheulie <79435002+saraheulie@users.noreply.github.com>
2021-03-22 23:16:24 -0500

    update

--- forms/CustomerAdd/CustomerAdd.js
+++ forms/CustomerAdd/CustomerAdd.js
@@ -1,14 +1,58 @@
-/*
-let name = inptName.value
-    let animalType = inptType.value
-    let query = "INSERT INTO pets (`petName`,`petType`) VALUES ('" + name + "', '" + animalType + "')"
-    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + databaseSchema + "&query=" + query)
+customerAdd.onshow = function() {
+  query = "SELECT * FROM customer"
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+
+    if (req.status == 200) { //transit trip worked. 
+        results = JSON.parse(req.responseText)
+        console.log(`The results are /n ${results}`)
+        if (results.length === 0)    
+           lblMessageAdd.value = "There are no customers in the database."
+        else {        
+           let message = ""
+           for (i = 0; i < results.length; i++)
+               message = message + results[i][1] + "\n"
+           txtaCustomerNamesAdd.value = message
+        } // end else
+
+    } else   // the transit didn't work - bad wifi? server turned off?
+        lblMessageAdd.value = "Error code: " + req.status
+}
+
+
+
+btnAdd.onclick=function(){
+    let name = inptCustomerName.value
+    let street = inptCustomerStreet.value
+    let city = inptCustomerCity.value
+    let state = inptCustomerState.value
+    let zipcode = inptCustomerZIP.value
+    
+    let query = "INSERT INTO customer (name, street, city, state, zipcode) VALUES ('" + name + "', '" + street + "', '" + city + "','" + state + "','" + zipcode + "')"
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
     if (req.status == 200) { 
         if (req.responseText == 500)    
-            lblMessage3.textContent = "You have successfully added the pet!"
+            lblMessageAdd.value = "You have successfully added the new customer! /n See the new customer in the updated list above."
         else
-            lblMessage3.textContent = "There was a problem with adding the pet to the database."
+            lblMessageAdd.value = "There was a problem with adding the customer to the database."
     } else 
-        lblMessage3.textContent = "Error: " + req.status
+        lblMessageAdd.textContent = "Error: " + req.status
         
-        */
+    query = "SELECT * FROM customer"
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+
+    if (req.status == 200) { //transit trip worked. 
+        results = JSON.parse(req.responseText)
+        console.log(`The results are /n ${results}`)
+        if (results.length === 0)    
+           lblMessageAdd.value = "There are no customers in the database."
+        else {        
+           let message = ""
+           for (i = 0; i < results.length; i++)
+               message = message + results[i][1] + "\n"
+           txtaCustomerNamesAdd.value = message
+        } // end else
+
+    } else   // the transit didn't work - bad wifi? server turned off?
+        lblMessageAdd.value = "Error code: " + req.status
+  
+}
--- forms/CustomerUpdate/CustomerUpdate.js
+++ forms/CustomerUpdate/CustomerUpdate.js
@@ -1,19 +1,47 @@
-/*let newName = inptNewName.value
+customerUpdate.onshow = function() {
+  query = "SELECT * FROM customer"
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+
+    if (req.status == 200) { //transit trip worked. 
+        results = JSON.parse(req.responseText)
+        console.log(`The results are /n ${results}`)
+        if (results.length === 0)    
+           lblMessageUpdate.value = "There are no customers in the database."
+        else {        
+           let message = ""
+           for (i = 0; i < results.length; i++)
+               message = message + results[i][1] + "\n"
+           txtaCustomerUpdate.value = message
+        } // end else
+
+    } else   // the transit didn't work - bad wifi? server turned off?
+        lblMessageUpdate.value = "Error code: " + req.status
+}
+
+
+
+
+btnCustomerUpdate.onclick=function(){
+  let newName = inptNewName.value
     let oldName = inptOldName.value
-    query = "SELECT * FROM pets WHERE `petName` = '" + oldName + "'"
-    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + databaseSchema + "&query=" + query)
+    
+    query = "SELECT * FROM customer WHERE `name` = '" + oldName + "'"
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
     if (req.status == 200) {
-        allPetData = JSON.parse(req.responseText)
-        if (allPetData.length > 0) {
-            query = "UPDATE pets SET `petName` ='" + newName + "' WHERE `petName` = '" + oldName + "'"
-            req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + databaseSchema + "&query=" + query)
+        results = JSON.parse(req.responseText)
+        if (results.length > 0) {
+            query = "UPDATE customer SET `name` ='" + newName + "' WHERE `name` = '" + oldName + "'"
+            req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
             if (req.status ==  200)  
                 if (req.responseText == 500)   
-                    lblMessage7.textContent = `You have successfully updated ${oldName} to ${newName}.`
+                    lblMessageUpdate.value = `You have successfully updated ${oldName} to ${newName}.`
                 else
-                    lblMessage7.textContent = `There was a problem updating ${oldName} to ${newName}.`
+                    lblMessageUpdate.value = `There was a problem updating ${oldName} to ${newName}.`
             else   
-                lblMessage7.textContent = `Error: ${req.status}`
+                lblMessageUpdate.value = `Error: ${req.status}`
         }
     } 
-    */
+}
+
+
+
